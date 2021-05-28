ac2d2fb46b3bfcbdf0d4c4a34a74d647201b6136
TvanMeer <tvanmeer123@gmail.com>
2021-05-28 08:50:42 +0200

    Connect with Binance

--- bbot/__init__.py
+++ bbot/__init__.py
@@ -4,8 +4,10 @@ Minimal market data fetcher in the making...
 
 """
 import os
+import asyncio
+import json
 
-options = {}
+from binance import AsyncClient
 
 
 class Coin():
@@ -18,23 +20,46 @@ class Coin():
 class DataBase():
     """All realtime data hold in memory. Coin class instances and meta."""
 
-    def __init__(self, options):
-        pass
+    def __init__(self):
+        self.base_assets = None
+        self.quote_assets = None
+        self.intervals = None
+        self.windowsize = None
 
 
-class Api():
+class _Api():
     """Interface with Binance through binance-python"""
 
-    def __init__(self, api_key, api_secret):
+    def __init__(self, api_key, api_secret, db):
         self.key = api_key
         self.secret = api_secret
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.start(db))
+
+    async def start(self, db):
+        client = await AsyncClient.create(self.key, self.secret)
+        res = await client.get_exchange_info()
+        print(json.dumps(res, indent=2))
+        await client.close_connection()
 
 
 class BinanceBot():
     """Public API exposed by bbot."""
 
-    def __init__(self, api_key, api_secret, options):
-        self.api = Api(api_key, api_secret)
+    def __init__(self, api_key, api_secret,
+                 base_assets=["BTC", ],
+                 quote_assets=["USDT", ],
+                 intervals=["1m", ],
+                 windowsize=200
+                 ):
+
+        self.data = DataBase()
+        self.data.base_assets = base_assets
+        self.data.quote_assets = quote_assets
+        self.data.intervals = intervals
+        self.data.windowsize = windowsize
+
+        self._api = _Api(api_key, api_secret, self.data)
 
 
 if __name__ == "__main__":
