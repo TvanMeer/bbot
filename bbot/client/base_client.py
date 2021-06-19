54806baf6f70c92ecff0de7b27fa98d822836e57
TvanMeer <tvanmeer123@gmail.com>
2021-06-19 16:37:13 +0200

    Fix return type of history functions

--- bbot/client/base_client.py
+++ bbot/client/base_client.py
@@ -1,5 +1,6 @@
 import abc
 import asyncio
+from abc import abstractmethod, ABCMeta
 from typing import Any, Dict, List, Set
 
 
@@ -8,10 +9,11 @@ from ..data.database   import Database
 from ..data.user_event import UserEvent
 from ..options         import Options
 
-class BaseClient(metaclass=abc.ABCMeta):
+class BaseClient(metaclass=ABCMeta):
     """Abstract interface for all exchange clients.
     Only for Binance a client is implemented.
     """
+
 
     def __init__(self, options: Options):
         """All bootstrapping logic of client is defined here.
@@ -22,6 +24,7 @@ class BaseClient(metaclass=abc.ABCMeta):
         4) Discover all pairs on the exchange and make a selection
         5) Start all downloads and streams
         """
+
         # 1
         self.options = options
         self.db = self._create_database(self.options)
@@ -43,29 +46,33 @@ class BaseClient(metaclass=abc.ABCMeta):
         It needs to be initialized in the separate process where
         the API client lives. Not in the main process.
         """
+
         return Database(options)
 
-    @abc.abstractmethod
+    @abstractmethod
     async def _create_async_client(options: Options) -> Any:
         """Returns some client object. In the case of Binance this
         is the python-binance.AsyncClient object. This object is
         passed as an argument in all functions that do API calls.
         """
+
         raise NotImplementedError
     
-    @abc.abstractmethod
+    @abstractmethod
     async def _download_all_symbols(self, client: Any) -> Any:
         """Downloads *some data* that contains all symbols of the 
         pairs being traded at the exchange. An example of a symbol 
         is 'BTCUSDT'. Return this data as provided by the API.
         """
+
         raise NotImplementedError
 
-    @abc.abstractmethod
+    @abstractmethod
     def _parse_all_symbols(raw: Any) -> Set[str]:
         """Filters all sybols from the data that is obtained 
         with _download_all_symbols().
         """
+
         raise NotImplementedError
     
     async def _start_coroutines(self, symbols: Set[str], client: Any) -> None:
@@ -74,36 +81,49 @@ class BaseClient(metaclass=abc.ABCMeta):
         _us   = asyncio.create_task(self._start_user_socket(client))
         await _cs, _hist, _us
 
-    @abc.abstractmethod
-    async def _download_history(symbols: Set[str], client: Any) -> Any:
-        """Returns historical candlestick data, as raw data in the
-        format provided by the API.
+
+    @abstractmethod
+    async def _download_history(symbols: Set[str], client: Any) -> None:
+        """Downloads all windows of historical candlestick data, 
+        as raw data in the format provided by the API. Then passes these
+        windows to _parse_history().
         """
+    
         raise NotImplementedError
 
-    @abc.abstractmethod
-    def _parse_history(raw: Any) -> List[Candle]:
-        """Takes raw historical candlestick data from _download_history()
-        and transforms it to a list of Candle objects.
+
+    @abstractmethod
+    def _parse_history(raw: List[Any]) -> None:
+        """Takes window of raw historical candlestick data from 
+        _download_history() and transforms it to a list of Candle objects.
+        Then passes it do db.route_history().
         """
+
         raise NotImplementedError
 
-    @abc.abstractmethod
+
+    @abstractmethod
     async def _start_candle_sockets(symbols: Set[str], client: Any) -> None:
         """Starts one or multiple websockets that stream candlestick data."""
+        
         raise NotImplementedError
 
-    @abc.abstractmethod
+
+    @abstractmethod
     def _parse_candle(raw: Dict) -> Candle:
         """Takes raw data of a single candle and returns a Candle object."""
+        
         raise NotImplementedError
 
-    @abc.abstractmethod
+
+    @abstractmethod
     async def _start_user_socket(client: Any) -> None:
         """Starts a websocket that listens to user events."""
+        
         raise NotImplementedError
 
-    @abc.abstractmethod
+
+    @abstractmethod
     def _parse_user_event(event: Any) -> UserEvent:
         """Parses a message from a user socket. This message is one of
         the following events:
@@ -111,4 +131,5 @@ class BaseClient(metaclass=abc.ABCMeta):
         2) Order update
         3) Trade update
         """
+        
         raise NotImplementedError
--- bbot/client/binance_client.py
+++ bbot/client/binance_client.py
@@ -9,13 +9,16 @@ from ..data.user_event import UserEvent
 
 class BinanceClient(BaseClient):
     """Binance client implementation"""
+
     
     def __init__(self, options: Options):
         """Starts bootstrapping logic in parents constructor."""
+
         super().__init__(options)
 
     async def _create_async_client(options: Options) -> AsyncClient:
         """Returns a python-binance.AsyncClient object."""
+
         client = await AsyncClient.create(api_key    = options.api_key, 
                                           api_secret = options.api_secret)
         return client
@@ -24,6 +27,7 @@ class BinanceClient(BaseClient):
         """Downloads ticker data that contains all symbols of trading pairs
         on Binance. This is a list of dicts with k=symbol and v=close_price.
         """
+
         raw = await client.get_all_tickers()
         return raw
 
@@ -32,6 +36,7 @@ class BinanceClient(BaseClient):
         """Filters symbols from Binance ticker data and returns a set 
         of symbols.
         """
+
         all_symbols = set()
         [all_symbols.add(t['symbol']) for t in raw]
         filtered_symbols = set()
@@ -43,39 +48,49 @@ class BinanceClient(BaseClient):
                         filtered_symbols.add(s)
 
         return filtered_symbols
-        
 
-    async def _download_history(symbols: Set[str], client: AsyncClient) -> List[List]:
-        """Downloads historical candlestick data from Binance and returns
-        this data without modifying it."""
-        pass #TODO
 
-    def _parse_history(raw: Any) -> List[Candle]:
-        """Takes raw historical candlestick data from _download_history()
-        and transforms it to a list of Candle objects.
+    async def _download_history(symbols: Set[str], client: AsyncClient) -> None:
+        """Downloads all windows of historical candlestick data, 
+        as raw data in the format provided by the API. Then passes these
+        windows to _parse_history().
         """
-        pass #TODO
+
+        pass #TODO: return none en pipe door _parse_history()
+
+    def _parse_history(raw: List[Any]) -> None:
+        """Takes window of raw historical candlestick data from 
+        _download_history() and transforms it to a list of Candle objects.
+        Then passes it to db.route_history().
+        """
+
+        pass #TODO: self.db.
 
     async def _start_candle_sockets(symbols: Set[str], client: AsyncClient) -> None:
         """Starts one or multiple websockets that stream candlestick data.
         Each socket streams data related to one pair. Only time interval 
         1 minute is streamed. Other intervals are calculated on the fly later.
         """
+
         pass #TODO
 
     def _parse_candle(raw: Dict) -> Candle:
         """Takes raw candle data from a single candle and returns a Candle object."""
+        
         pass #TODO
 
     async def _start_user_socket(client: AsyncClient) -> None:
         """Starts a websocket that listens to user events."""
+        
         pass #TODO
 
     def _parse_user_event(event: Any) -> UserEvent:
+        
         """Parses a message from a user socket. This message is one of
         the following events:
         1) Account update
         2) Order update
         3) Trade update
         """
+
         pass #TODO
