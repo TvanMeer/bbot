651dcbb7d54b57df504a3010e82543302524eb90
TvanMeer <tvanmeer123@gmail.com>
2021-06-30 04:57:11 +0200

    Update base client test interface

--- tests/test_client/test_base_client.py
+++ tests/test_client/test_base_client.py
@@ -14,101 +14,97 @@ def options():
     return Options()
 
 
-class TestClient(_BaseClient):
-
-    # A mock client implementation:
-    def __init__(self, options: Options):
-        super().__init__(options)
-
-    def shutdown(self) -> None:
-        pass
-
-    async def create_async_client(self, options: Options) -> Any:
-        return None
-
-    async def download_all_symbols(self, client: Any) -> Any:
-        return [{"BTCUSDT": 1000}, {"ADAUSDT": 165}]
-
-    def parse_all_symbols(self, raw: Any) -> FrozenSet[str]:
-        return frozenset(["BTCUSDT", "ADAUSDT"])
-
-    async def download_history(
-        self, symbols: FrozenSet[str], client: Any, db: _Database
-    ) -> None:
-        await asyncio.sleep(1)
-
-    def parse_historical_candle(
-        self, raw: Any, symbol: str, interval: str, db: _Database
-    ) -> None:
-        pass
-
-    async def start_candle_sockets(
-        self, symbols: FrozenSet[str], client: Any, db: _Database
-    ) -> None:
-        await asyncio.sleep(1)
-
-    def parse_candle(self, raw: Any) -> Candle:
-        return Candle(
-            event_time=None,
-            symbol="BTCUSDT",
-            open_time=datetime,
-            close_time=datetime,
-            is_closed=False,
-            open_price=123.0,
-            close_price=123.0,
-            high_price=123.0,
-            low_price=123.0,
-            base_asset_volume=10000.0,
-            n_trades=10,
-            quote_asset_volume=10000.0,
-            taker_buy_base_asset_volume=10000.0,
-            taker_buy_quote_asset_volume=10000.0,
-        )
-
-    async def start_user_socket(self, client: Any) -> None:
-        await asyncio.sleep(1)
-
-    def parse_user_event(self, event: Any) -> UserEvent:
-        return UserEvent()
-
-    async def start_consumer(self) -> None:
-        await asyncio.sleep(1)
-
-
 @pytest.fixture
-def base_client(options):
-    return TestClient(options)
-
-
-@pytest.fixture
-def event_loop():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    yield loop
-    loop.close()
-
-
-def test_init(options, base_client):
-    assert isinstance(base_client.create_database(options), _Database)
-    assert isinstance(base_client.loop, asyncio.BaseEventLoop)
-    assert base_client.db.selected_symbols == frozenset(
-        [
-            "BTCUSDT",
-        ]
+def all_symbols():
+    return frozenset(
+        ["BTCUSDT", "ADAUSDT", "BTCUSDC", "XRPBTC", "BTCxxxxxUSDT"]
     )
+
+
+@pytest.fixture
+def parsed_historical_candle():
+    return {}  # TODO
+
+
+@pytest.fixture
+def symbol():
+    return "BTCUSDT"
+
+
+@pytest.fixture
+def interval():
+    return "1m"
+
+
+@pytest.fixture
+def parsed_new_candle():
+    return {}  # TODO
+
+
+# @pytest.fixture
+# def event_loop():
+#    loop = asyncio.new_event_loop()
+#    asyncio.set_event_loop(loop)
+#    yield loop
+#    loop.close()
+
+# class TestClient(_BaseClient):
+
+#    # A mock client implementation:
+#    def __init__(self, options: Options()):
+#        super().__init__(options)
+
+# @pytest.fixture
+# def client(options):
+#    return TestClient(options)
+
+
+# Unit tests ----------------------------------------------------------
+
+
+def test_init(options):
+    pass
+
+
+def test_start():
+    pass
+
+
+def test_stop():
+    pass
+
+
+def test_start_loops():
+    pass
 
 
 @pytest.mark.asyncio
-async def test_start_coroutines(event_loop, base_client):
+async def test_start_coroutines():
+    pass
 
-    assert event_loop.is_running()
 
-    before = time.time()
-    await base_client.start_coroutines(
-        event_loop,
-        base_client.db.selected_symbols,
-        base_client.client,
-        base_client.db,
-    )
-    after = time.time()
-    assert after - before > 1 and after - before < 2
+@pytest.mark.asyncio
+async def test_start_consumer():
+    pass
+
+
+def test_filter_symbols(all_symbols, options):
+    pass
+
+
+def test_validate_historical_candle(
+    parsed_historical_candle, symbol, interval
+):
+    pass
+
+
+def test_calc_window_rolls(parsed_new_candle, symbol):
+    pass
+
+
+def test_validate_new_candle(parsed_new_candle, symbol):
+    pass
+
+
+def test_update_candle(parsed_new_candle, symbol, interval):
+    pass
