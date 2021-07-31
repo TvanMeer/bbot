55abf786137dc8e2ff92ac970a1df7df872ca44b
TvanMeer <tvanmeer123@gmail.com>
2021-07-31 11:56:46 +0200

    Rewrite options class

--- src/bbot/options.py
+++ src/bbot/options.py
@@ -1,4 +1,4 @@
-from typing import Union, Dict, List, FrozenSet
+from typing import Union, Dict, List
 
 
 class Options:
@@ -16,7 +16,7 @@ class Options:
         windows: Dict[str, int] = {"1m": 500, "15m": 200},
     ):
 
-        self._possible_intervals = {
+        self._POSSIBLE_INTERVALS = {
             "2s": 2000,
             "1m": 60000,
             "3m": 180000,
@@ -33,72 +33,24 @@ class Options:
             "3d": 259200000,
             "1w": 604800000,
         }
-        self._possible_modes = frozenset(
-            ["DEBUG", "HISTORY", "STREAM", "PAPER", "TESTNET", "TRADE"]
-        )
+
+        self._POSSIBLE_MODES = {
+            "DEBUG",
+            "HISTORY",
+            "STREAM",
+            "PAPER",
+            "TESTNET",
+            "TRADE",
+        }
 
         self._api_key = api_key
         self._api_secret = api_secret
-        self._mode = self._verify_clean_mode(mode)
-        self._base_assets = self._verify_clean_base_assets(base_assets)
-        self._quote_assets = self._verify_clean_quote_assets(quote_assets)
-        self._windows = self._verify_clean_windows(windows)
+        self._mode = mode
+        self._base_assets = base_assets
+        self._quote_assets = quote_assets
+        self._windows = windows
 
-    def _verify_clean_mode(self, mode: str) -> str:
-        """Verifies raw user input for option `mode`"""
-
-        if mode.upper() in self._possible_modes:
-            return mode.upper()
-        else:
-            raise Exception(
-                f"Invalid input for option `mode` in bbot.Options: {mode}"
-            )
-
-    def _verify_clean_base_assets(
-        self, base_assets: Union[str, List[str]]
-    ) -> FrozenSet[str]:
-        """Verifies raw user input for option `base_assets`"""
-
-        e = "Invalid input for option `base_assets` or `quote_assets` in bbot.Options"
-
-        if type(base_assets) is str:
-            if (
-                base_assets.isalpha() and len(base_assets) < 10
-            ) or base_assets == "*":
-                return frozenset((base_assets.upper(),))
-            else:
-                raise Exception(e)
-        else:
-            l = len(base_assets)
-
-            if (
-                sum([b.isalpha() for b in base_assets]) == l
-                and sum([len(b) < 10 for b in base_assets]) == l
-            ):
-                return frozenset([b.upper() for b in base_assets])
-            else:
-                raise Exception(e)
-
-    def _verify_clean_quote_assets(
-        self, quote_assets: Union[str, List[str]]
-    ) -> FrozenSet[str]:
-        """Verifies raw user input for option `quote_assets`"""
-
-        return self._verify_clean_base_assets(quote_assets)
-
-    def _verify_clean_windows(self, windows: Dict[str, int]) -> Dict[str, int]:
-        """Verifies raw user input for option `windows`"""
-
-        if sum(
-            [iv in self._possible_intervals for iv in windows.keys()]
-        ) == len(windows):
-            if sum([w <= 500 for w in windows.values()]) == len(windows):
-                if sum([w % 2 == 0 for w in windows.values()]) == len(windows):
-                    return windows
-
-        raise Exception("Invalid input for option `windows` in bbot.Options")
-
-    # Getters and setters
+    # Getters
     @property
     def api_key(self) -> str:
         return self._api_key
@@ -112,37 +64,109 @@ class Options:
         return self._mode
 
     @property
-    def base_assets(self) -> FrozenSet[str]:
+    def base_assets(self) -> set[str]:
         return self._base_assets
 
     @property
-    def quote_assets(self) -> FrozenSet[str]:
+    def quote_assets(self) -> set[str]:
         return self._quote_assets
 
     @property
     def windows(self) -> Dict[str, int]:
         return self._windows
 
-    @property
-    def possible_intervals(self) -> Dict[str, int]:
-        return self._possible_intervals
+    # Setters
+    def _error(self, attr_name):
+        raise Exception(
+            f"Invalid input for option {attr_name} in bbot.Options"
+        )
 
-    @property
-    def possible_modes(self) -> FrozenSet[str]:
-        return self._possible_modes
+    def _validate_credential(self, attr_name, cred):
+        if cred == " ":
+            pass
+        elif len(cred) != 64:
+            self._error(attr_name)
+        for c in cred:
+            if not c.isalpha() or c.isdigit():
+                self._error(attr_name)
+
+    @api_key.setter
+    def api_key(self, key):
+        self._validate_credential("api_key", key)
+        self._api_key = key
+
+    @api_secret.setter
+    def api_secret(self, secret):
+        self._validate_credential("api_secret", secret)
+        self._api_secret = secret
 
     @mode.setter
-    def mode(self, mode: str) -> None:
-        self._mode = self._verify_clean_mode(mode)
+    def mode(self, mode: str):
+        m = mode.upper()
+        if m in self._POSSIBLE_MODES:
+            self._mode = m
+        else:
+            self._error("mode")
+
+    def _validate_asset_str(self, attr, a):
+        checks = [
+            type(a) is str,
+            a.isalpha(),
+            len(a) < 10,
+        ]
+        if sum(checks) == 3:
+            return a.upper()
+        else:
+            self._error(attr)
+
+    def _set_assets(self, attr, a):
+        def validate(attr, s):
+            return self._validate_asset_str(attr, s)
+
+        def set_a(s):
+            setattr(self, "_" + attr, s)
+
+        if a == "*":
+            set_a("*")
+        elif type(a) is str:
+            v = validate(attr, a)
+            set_a(set(v))
+        elif type(a) is list:
+            res = set()
+            [res.add(validate(attr, asset)) for asset in a]
+            set_a(res)
+        else:
+            self._error(attr)
 
     @base_assets.setter
-    def base_assets(self, base_assets: Union[str, List[str]]) -> None:
-        self._base_assets = self._verify_clean_base_assets(base_assets)
+    def base_assets(self, base_assets: Union[str, List[str]]):
+        self._set_assets("base_assets", base_assets)
 
     @quote_assets.setter
-    def quote_assets(self, quote_assets: Union[str, List[str]]) -> None:
-        self._quote_assets = self._verify_clean_quote_assets(quote_assets)
+    def quote_assets(self, quote_assets: Union[str, List[str]]):
+        if quote_assets == "*" and self.base_assets == "*":
+            raise Exception("Cannot use * for both base and quote assets")
+        self._set_assets("quote_assets", quote_assets)
 
     @windows.setter
-    def windows(self, windows: Dict[str, int]) -> None:
-        self._windows = self._verify_clean_windows(windows)
+    def windows(self, windows: Dict[str, int]):
+        intervals = set(windows.keys())
+        winsizes = set(windows.values())
+
+        def validate_interval(interval):
+            possible = set(self._POSSIBLE_INTERVALS.keys())
+            if not interval in possible:
+                self._error("window")
+
+        def validate_winsize(winsize):
+            checks = [
+                type(winsize) is int,
+                len(winsize) <= 500,
+                winsize % 2 == 0,
+            ]
+            if sum(checks) != 3:
+                self._error("window")
+
+        [validate_interval(iv) for iv in intervals]
+        [validate_winsize(w) for w in winsizes]
+        self._windows = windows
