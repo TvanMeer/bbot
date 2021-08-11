d9f951f7098c889e29dc2f4c8a3e467d8f63b808
TvanMeer <tvanmeer123@gmail.com>
2021-08-12 01:56:00 +0200

    Implement process func in processors

--- src/bbot/processor.py
+++ src/bbot/processor.py
@@ -1,14 +1,23 @@
 import logging
-from typing import Any
+from typing import Any, List
 
 from .database import Database
 from .options import Options
 
 
 class _BaseProcessor:
-    def process(
+    """Usage:
+
+    Processors are instantiated in separate coroutines inside the class AsyncBot.
+    All of them are used like this:
+
+    cp = _CandleProcessor()
+    self.database = cp.process(candle, self.database)
+    """
+
+    def pipe(
         self, datatype: str, raw_data: Any, database: Database
-    ) -> None:
+    ) -> Database:
         parsed_data = self.parse(raw_data)
         logging.debug(f"Parsed {datatype}.")
 
@@ -23,38 +32,53 @@ class _BaseProcessor:
 
         return updated_db
 
-    def parse(self, raw_data: Any) -> dict:
+    def process(self, raw_data: Any, database: Database) -> Database:
         raise NotImplementedError
 
-    def validate(self, parsed_data: dict, database) -> dict:
+    def parse(self, raw_data: Any) -> Any:
         raise NotImplementedError
 
-    def filterr(self, validated_data: dict, options: Options) -> dict:
+    def validate(self, parsed_data: Any, database: Database) -> Any:
         raise NotImplementedError
 
-    def insert(self, filtered_data: dict, database: Database) -> Database:
+    def filterr(self, validated_data: Any, options: Options) -> Any:
+        raise NotImplementedError
+
+    def insert(self, filtered_data: Any, database: Database) -> Database:
         raise NotImplementedError
 
 
 class _SymbolProcessor(_BaseProcessor):
-    pass
+    def process(self, tickers_list: List, database: Database) -> Database:
+        return super().pipe("symbol", tickers_list, database)
 
 
 class _HistoricalCandleProcessor(_BaseProcessor):
-    pass
+    def process(self, historical_candle: List, database: Database) -> Database:
+        return super().pipe(
+            "historical_candle", historical_candle, database
+        )
 
 
 class _StreamedCandleProcessor(_BaseProcessor):
-    pass
+    def process(self, candle: dict, database: Database) -> Database:
+        return super().pipe("candle", candle, database)
 
 
 class _StreamedMiniTickerProcessor(_BaseProcessor):
-    pass
+    def process(self, minitick: dict, database: Database) -> Database:
+        return super().pipe("minitick", minitick, database)
 
 
 class _StreamedTickerProcessor(_BaseProcessor):
-    pass
+    def process(self, tick: dict, database: Database) -> Database:
+        return super().pipe("tick", tick, database)
 
 
 class _DepthProcessor(_BaseProcessor):
-    pass
+    def __init__(self, resolution):
+        self.resolution = resolution
+        super().__init__()
+
+    def process(self, depthchart: List, database: Database) -> Database:
+        return super().pipe("depth" + self.resolution, depthchart, database)
