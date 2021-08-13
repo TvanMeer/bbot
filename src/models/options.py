c7edd671c3b93716e87064fbacc218aa15fcc6aa
TvanMeer <tvanmeer123@gmail.com>
2021-08-13 05:40:38 +0200

    Add typing to pydantic options class

--- src/models/options.py
+++ src/models/options.py
@@ -1,16 +1,23 @@
+from typing import Callable, List, Literal, Optional, Union
 from pydantic import BaseModel
+from pydantic.types import DirectoryPath, SecretStr
+
 
 class Options(BaseModel):
     """Contains all optional arguments for Bbot.
     Required by Bot object at initialization.
     """
 
-    key              = " "
-    secret           = " "
-    mode             = "TEST"
-    datadir          = "./bbot_data/"
-    base_assets      = ["BTC", "HOT"]
-    quote_assets     = ["USDT"]
-    window_intervals = ["2s", "1m"]
-    window_length    = 200
-    datasources      = ["candle", "depth20", "ticker"]
+    key:              SecretStr                                 = " "
+    secret:           SecretStr                                 = " "
+    mode:             Literal[
+                        "DEBUG", "TEST", "HISTORY", 
+                        "STREAM", "PAPER", "TRADE"
+                      ]                                         = "TEST"
+    datadir:          Optional[DirectoryPath]                   = None
+    base_assets:      Union[str, List[str]]                     = ["BTC", "HOT"]
+    quote_assets:     Union[str, List[str]]                     = ["USDT"]
+    window_intervals: Union[str, List[str]]                     = ["2s", "1m"]
+    window_length:    int                                       = 200
+    datasources:      Union[str, List[str]]                     = ["candle", "depth5", "miniticker"]
+    features:         Optional[Union[Callable, List[Callable]]] = None
