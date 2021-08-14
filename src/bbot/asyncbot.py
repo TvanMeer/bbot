88584f60ee2db4bf06272d885b3dc20c51fd7069
TvanMeer <tvanmeer123@gmail.com>
2021-08-14 14:32:45 +0200

    Write asyncbot constructor

--- src/bbot/asyncbot.py
+++ src/bbot/asyncbot.py
@@ -4,110 +4,98 @@ from binance.client import AsyncClient
 
 class _AsyncBot:
     """Holds internal implementation of bot.
-    Can run in separate process of thread.
+    Can run in separate process or thread.
     """
 
     def __init__(self, options):
 
         self.options = options
-        self.candles = {}
-        self.user_events = []
 
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self.loop)
+        t = lambda coro: asyncio.create_task(coro)
 
-        self.shutdown_flag = False
-        self.finished_history_download = set()
+        t1 = t(self.prepare_connection)
+        t2 = t(self.prepare_database)
+        t3 = t(self.download_history)
 
-        if options.mode != "DEBUG":
-            self.loop.run_until_complete(self.prepare)
-            self.loop.run_until_complete(self.start_loops)
+        # Infinite loops
+        t4 = t(self.consumer)
+        t5 = t(self.candle_producer)
+        t6 = t(self.miniticker_producer)
+        t7 = t(self.ticker_producer)
+        t8 = t(self.depth_producer)
+        t9 = t(self.userevent_producer)
 
-    async def prepare(self):
+        mode_filter = {
+            "test":    None,
+            "debug":   {t1, t2},
+            "history": {t1, t2},
+            "stream":  {t1, t2, t4},
+            "paper":   {t1, t2, t4, t9},
+            "trade":   {t1, t2, t4, t9},
+        }
+        streams_filter = {
+            "candle":     {t3, t4, t5}, 
+            "miniticker": {t4, t6},
+            "ticker":     {t4, t7},
+            "depth5":     {t4, t8},
+            "depth10":    {t4, t8},
+            "depth20":    {t4, t8},
+        }
 
-        self.client = await AsyncClient.create(
-            api_key=self.options.api_key,
-            api_secret=self.options.api_secret,
-            testnet=self.options.mode == "DEBUG",
-        )
-        self.all_symbols = await self.download_all_symbols(self.client)
-        self.selected_symbols = self.select_symbols(
-            self.all_symbols, self.options
-        )
-        self.q = asyncio.Queue(
-            loop=self.loop, maxsize=len(self.selected_symbols)
-        )
-        self.exchange_info = await self.download_exchange_info(self.client)
-        self.account_info = await self.download_account_info(self.client)
+        selected_mode = mode_filter[options.mode]
+        selected_streams = set()
+        [selected_streams.add(streams_filter[s]) for s in options.streams]
+        selected_tasks = selected_mode.union(selected_streams)
+        
 
-    async def download_all_symbols(self, client):
-        tickers = await client.get_all_tickers()
-        all_symbols = set()
-        [all_symbols.add(t["symbol"]) for t in tickers]
-        return all_symbols
+        # Shared variables
+        self.client = None
+        self.database = None
+        self.queue = asyncio.Queue()
 
-    def select_symbols(self, all_symbols, options):
+        # State variables
+        self.t1_complete = False
+        self.t2_complete = False
+        self.t3_complete = False
 
-        selected = set()
+        if selected_tasks is not None:
+            asyncio.gather(*selected_tasks)
 
-        def exact_match(b, q):
 
-            if "*" not in [b, q]:
-                left = s[: len(b)]
-                right = s[len(b) :]
-                if left == b and right == q:
-                    selected.add(s)
-            else:
-                selected.add(s)
-
-        for s in all_symbols:
-            for b in options.base_assets:
-                if s.startswith(b) or b == "*":
-                    for q in options.quote_assets:
-                        if s.endswith(q) or q == "*":
-                            exact_match(b, q)
-
-        return selected
-
-    async def download_exchange_info(self, client):
-
-        info = await client.get_exchange_info()
-        return info
-
-    async def download_account_info(self, client):
-
-        info = await client.get_account()
-        return info
-
-    async def start_loops(self):
+    async def prepare_connection(self):
         pass
 
-    async def public_interface_listener(self):
-        pass
+    async def prepare_database(self):
+        while not self.t1_complete:
+            asyncio.sleep(0.1)
 
-    async def history_downloader(self):
-        pass
+    async def download_history(self):
+        while not self.t2_complete:
+            asyncio.sleep(0.1)
+        if "candle" in self.options.streams:
+            pass
 
-    async def candle_streamer(self):
-        pass
 
-    async def user_event_listener(self):
-        pass
+    # Infinite loops --------------------------------------------------------
 
     async def consumer(self):
         pass
 
-    async def log_raw_data(self, event):
-        pass
+    async def candle_producer(self):
+        while not self.t3_complete:
+            asyncio.sleep(0.01)
 
-    def dispatch_event(self, event):
-        pass
+    async def miniticker_producer(self):
+        while not self.t3_complete:
+            asyncio.sleep(0.01)
 
-    def handle_public_request(self, public_interface_request):
-        pass
+    async def ticker_producer(self):
+        while not self.t3_complete:
+            asyncio.sleep(0.01)
 
-    def handle_candle(self, candle):
-        pass
+    async def depth_producer(self):
+        while not self.t3_complete:
+            asyncio.sleep(0.01)
 
-    def handle_user_event(self, user_event):
+    async def userevent_producer(self):
         pass
