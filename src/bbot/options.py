511372113b0bd819c55a7221db9f742abccb6ce5
TvanMeer <tvanmeer123@gmail.com>
2021-08-14 23:50:34 +0200

    Cleanup pydantic options model

--- src/bbot/options.py
+++ src/bbot/options.py
@@ -1,4 +1,6 @@
-from typing import Callable, List, Literal, Optional, Union
+from typing import Callable, Iterable, Optional, Union
+from enum import Enum
+
 from pydantic import BaseModel, validator
 from pydantic.types import DirectoryPath, PositiveInt, SecretStr
 
@@ -8,22 +10,58 @@ class Options(BaseModel):
     Required by Bot object at initialization.
     """
 
-    key:              SecretStr                                 = " "
-    secret:           SecretStr                                 = " "
-    mode:             Literal[
-                        "DEBUG", "TEST", "HISTORY", 
-                        "STREAM", "PAPER", "TRADE"
-                      ]                                         = "TEST"
-    datadir:          Optional[DirectoryPath]                   = None
-    base_assets:      Union[str, List[str]]                     = ["BTC", "HOT"]
-    quote_assets:     Union[str, List[str]]                     = ["USDT"]
-    window_intervals: Union[str, List[str]]                     = ["2s", "1m"]
-    window_length:    PositiveInt                               = 200
-    streams:          Union[str, List[str]]                     = ["candle", "depth5", "miniticker"]
-    features:         Optional[Union[Callable, List[Callable]]] = None
+    class Mode(str, Enum):
+        test       = "test"
+        debug      = "debug"
+        history    = "history"
+        stream     = "stream"
+        paper      = "paper"
+        trade      = "trade"
+
+
+    class Stream(str, Enum):
+        candle     = "candle"
+        miniticker = "miniticker"
+        ticker     = "ticker"
+        depth5     = "depth5"
+        depth10    = "depth10"
+        depth20    = "depth20"
+
+
+    class Interval(str, Enum):
+        second_2   = "2s"
+        minute_1   = "1m"
+        minute_3   = "3m"
+        minute_5   = "5m"
+        minute_15  = "15m"
+        minute_30  = "30m"
+        hour_1     = "1h"
+        hour_2     = "2h"
+        hour_4     = "4h"
+        hour_6     = "6h"
+        hour_8     = "8h"
+        hour_12    = "12h"
+        day_1      = "1d"
+        day_3      = "3d"
+        week_1     = "1w"
+
+
+
+    key:              SecretStr                                     = " "
+    secret:           SecretStr                                     = " "
+    mode:             Mode                                          = Mode.test
+    datadir:          Optional[DirectoryPath]                       = None
+    base_assets:      Union[str, Iterable[str]]                     = ["BTC", "HOT"]
+    quote_assets:     Union[str, Iterable[str]]                     = ["USDT"]
+    window_intervals: Union[Interval, Iterable[Interval]]           = [Interval.second_2, Interval.minute_1]
+    window_length:    PositiveInt                                   = 200
+    streams:          Union[Stream, Iterable[Stream]]               = [Stream.candle, Stream.depth5, Stream.miniticker]
+    features:         Optional[Union[Callable, Iterable[Callable]]] = None
+
 
 
     @validator("key", "secret")
+    @classmethod
     def check_credentials(cls, v):
         assert v == " " or len(v) == 64, "Both key and secret should be of length 64."
         assert v == " " or v.isalnum(), "Both key and secret should be alphanumeric."
@@ -31,35 +69,12 @@ class Options(BaseModel):
 
 
     @validator("base_assets", "quote_assets")
+    @classmethod
     def check_asset_names(cls, v):
         def check_str(s):
-            assert len(s) in range(3, 10), "Asset names should be between 3 and 6 characters long."
+            assert len(s) in range(3, 6), "Asset names should be between 3 and 6 characters long."
             assert s.isalnum(), "Asset names should be alphanumeric, like `BTC` or `USDC`."
-        if v is str:
-            check_str(v)
-            return v.upper()
-        else:
-            [check_str(s) for s in v]
-            return list(map(str.upper, v))
 
-
-    @validator("window_intervals")
-    def check_window_intervals(cls, v):
-        def check_str(s):
-            assert s.isalnum(), "Window intervals should be alphanumeric, like `1m` or `4h`."
-            assert v[-1].lower() in {"s", "m", "h", "d", "w"}, "Window intervals should end with `s`, `m`, `h`, `d` or `w`."
-            if v[-1] == "s":
-                assert int(v[:-1]) % 2 == 0, "Window second interval should be divisible by 2."
-        if v is str:
-            check_str(v)
-            return v
-        else:
-            [check_str(s) for s in v]
-            return v
-
-
-    @validator("streams")
-    def check_streams(cls, v):
-        allowed = {"candle", "miniticker", "ticker", "depth5", "depth10", "depth20"}
-        assert set(v).issubset(allowed), f"Valid streams are: {allowed}"
-        return v
+        asset_names = v.lower() if v is type(str) else list(map(str.lower, v))
+        map(check_str, asset_names)
+        return asset_names
