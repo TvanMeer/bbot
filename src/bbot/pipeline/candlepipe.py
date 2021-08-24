6de5ed3698ecaaba4c0ba6ecc04ad1270b24309e
TvanMeer <tvanmeer123@gmail.com>
2021-08-25 00:16:35 +0200

    Add candle pipeline boilerplate

--- src/bbot/pipeline/candlepipe.py
+++ src/bbot/pipeline/candlepipe.py
@@ -17,13 +17,36 @@ class CandleHistoryPipeline(Pipeline):
         pass
 
     def parse(self, raw: List) -> Candle:
-        """Turns historical candle to a Candle object."""
+        """Turns historical candle into a Candle object."""
 
         pass
 
     def insert(self, candle: Candle, window: Window) -> DataBase:
         """Inserts the historical candle in the corresponding window.
         Then inserts the window in the database and returns the updated database.
+        """
+
+        pass
+
+
+
+class CandlePipeline(Pipeline):
+
+    def get_window(self, raw: dict, db: DataBase) -> Window:
+        """Receives a candle as a dictionary from a websocket.
+        Finds and returns the corresponding window in the database.
+        """
+
+        pass
+
+    def parse(self, raw: dict) -> Candle:
+        """Turns latest candle from websocket into a Candle object."""
+
+        pass
+
+    def insert(self, candle: Candle, window: Window) -> DataBase:
+        """Inserts the new candle in the corresponding window.
+        Then inserts this window in the database and returns the updated database.
         """
 
         pass
