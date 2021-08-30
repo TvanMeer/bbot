5405dfcb9c928839b6d92820bd733c387dedef59
TvanMeer <tvanmeer123@gmail.com>
2021-08-30 02:37:19 +0200

    Implement candle update and create functions

--- src/models/candle.py
+++ src/models/candle.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from typing import List
+from typing import List, Type, TypeVar
 
 from binance import AsyncClient, BinanceSocketManager
 from pydantic import BaseModel
@@ -9,6 +9,8 @@ from pydantic.types import PositiveInt, condecimal
 
 from .database import ContentType
 from .options import Options
+
+_Candle = TypeVar("_Candle", bound="Candle")
 
 
 class Candle(BaseModel):
@@ -78,43 +80,110 @@ class Candle(BaseModel):
 
 
     @staticmethod
-    def parse_candle(raw_candle: dict):
-      """Returns Candle instance."""
+    def parse_candle(raw_candle: dict) -> _Candle:
+        """Parse websocket candlestick, return Candle instance."""
 
-      try:
-        return Candle(
-          open_price         =raw_candle["o"], 
-          close_price        =raw_candle["c"],
-          high_price         =raw_candle["h"],
-          low_price          =raw_candle["l"],
-          base_volume        =raw_candle["v"],
-          quote_volume       =raw_candle["q"],
-          base_volume_taker  =raw_candle["V"],
-          quote_volume_taker =raw_candle["Q"],
-          n_trades           =raw_candle["n"]
-        )
-      except ValidationError as e:
-        logging.critical(e)
+        try:
+            return Candle(
+                open_price         =raw_candle["o"],
+                close_price        =raw_candle["c"],
+                high_price         =raw_candle["h"],
+                low_price          =raw_candle["l"],
+                base_volume        =raw_candle["v"],
+                quote_volume       =raw_candle["q"],
+                base_volume_taker  =raw_candle["V"],
+                quote_volume_taker =raw_candle["Q"],
+                n_trades           =raw_candle["n"],
+            )
+        except ValidationError as e:
+            logging.critical(e)
+
 
 
     @staticmethod
-    def parse_historical_candle(raw_candle: List):
-      """Returns Candle instance."""
+    def parse_historical_candle(raw_candle: List) -> _Candle:
+        """Parse historical candlestick, return Candle instance."""
 
-      try:
-        return Candle(
-          open_price         =raw_candle[1], 
-          close_price        =raw_candle[4],
-          high_price         =raw_candle[2],
-          low_price          =raw_candle[3],
-          base_volume        =raw_candle[5],
-          quote_volume       =raw_candle[7],
-          base_volume_taker  =raw_candle[9],
-          quote_volume_taker =raw_candle[10],
-          n_trades           =raw_candle[8]
-        )
-      except ValidationError as e:
-        logging.critical(e)
+        try:
+            return Candle(
+                open_price         =raw_candle[1],
+                close_price        =raw_candle[4],
+                high_price         =raw_candle[2],
+                low_price          =raw_candle[3],
+                base_volume        =raw_candle[5],
+                quote_volume       =raw_candle[7],
+                base_volume_taker  =raw_candle[9],
+                quote_volume_taker =raw_candle[10],
+                n_trades           =raw_candle[8],
+            )
+        except ValidationError as e:
+            logging.critical(e)
+
+
+
+    @staticmethod
+    def update(
+        candle:                 Type[_Candle],
+        update:                 Type[_Candle],
+        previous_update:        Type[_Candle],
+        previous_update_closed: bool,
+    ) -> _Candle:
+        """Updates and returns a candlestick."""
+
+        new_candle = candle
+        new_candle.close_price = update.close_price
+        new_candle.high_price  = max(candle.high_price, update.high_price)
+        new_candle.low_price   = min(candle.low_price, update.low_price)
+
+        if previous_update_closed:
+            new_candle.base_volume        += update.base_volume
+            new_candle.quote_volume       += update.quote_volume
+            new_candle.base_volume_taker  += update.base_volume_taker
+            new_candle.quote_volume_taker += update.quote_volume_taker
+            new_candle.n_trades           += update.n_trades
+        else:
+            new_candle.base_volume        += update.base_volume - previous_update.base_volume
+            new_candle.quote_volume       += update.quote_volume - previous_update.quote_volume
+            new_candle.base_volume_taker  += update.base_volume_taker - previous_update.base_volume_taker
+            new_candle.quote_volume_taker += update.quote_volume_taker - previous_update.quote_volume_taker
+            new_candle.n_trades           += update.n_trades - previous_update.n_trades
+
+        return new_candle
+
+
+
+    @staticmethod
+    def create_2s_candle(
+        update:                 Type[_Candle],
+        previous_update:        Type[_Candle],
+        previous_update_closed: bool,
+    ) -> _Candle:
+        """Produces a candle for the timeframe with an interval of 2 seconds."""
+
+        if previous_update_closed:
+          return Candle(
+            open_price         = previous_update.close_price,
+            close_price        = update.close_price,
+            high_price         = max(update.close_price, previous_update.close_price),
+            low_price          = min(update.close_price, previous_update.close_price),
+            base_volume        = update.base_volume,
+            quote_volume       = update.quote_volume,
+            base_volume_taker  = update.base_volume_taker,
+            quote_volume_taker = update.quote_volume_taker,
+            n_trades           = update.n_trades
+          )
+        else:
+          return Candle(
+            open_price         = previous_update.close_price,
+            close_price        = update.close_price,
+            high_price         = max(update.close_price, previous_update.close_price),
+            low_price          = min(update.close_price, previous_update.close_price),
+            base_volume        = update.base_volume - previous_update.base_volume,
+            quote_volume       = update.quote_volume - previous_update.quote_volume,
+            base_volume_taker  = update.base_volume_taker - previous_update.base_volume_taker,
+            quote_volume_taker = update.quote_volume_taker - previous_update.quote_volume_taker,
+            n_trades           = update.n_trades - previous_update.n_trades
+          )
 
 
 
@@ -126,11 +195,11 @@ class Candle(BaseModel):
         manager:       BinanceSocketManager,
         shutdown_flag: bool,
     ) -> None:
-        """Coroutine that streams candlestick data for a single symbol through a websocket.
+        """Coroutine that streams 1m candlestick data for a single symbol, through a websocket.
 
         Single candles are added to the queue, as tuple(symbol, interval, content_type, raw_candle)
         Streams are always 1 minute candles. Windows are programatically updated later.
-        This way bbot requires only one stream for multiple windows.
+        That way bbot requires just one stream for multiple windows.
         """
 
         socket = manager.kline_socket(symbol)
@@ -161,6 +230,7 @@ class Candle(BaseModel):
         """
 
         def gen_timestring(i, l):
+            # helperfunc
             amount = i[:-1]
             period = i[-1]
             total  = str(l * amount)
@@ -177,21 +247,24 @@ class Candle(BaseModel):
             async for raw_candle in await client.get_historical_klines_generator(
                 s.upper(), i, t
             ):
-                msg = (s, Options.Interval(i), ContentType.candle_history, raw_candle)
+                msg = (
+                    s,
+                    Options.Interval(i),
+                    ContentType.candle_history,
+                    raw_candle,
+                )
                 await queue.put(msg)
 
         for i in intervals:
-          interval = i.value
-          time = gen_timestring(interval, window_length)
-          if shutdown_flag:
-            return
-          elif i == Options.Interval.second_2:
-            continue
-          else:
-            await download_window(symbol, interval, time)
-            await queue.put(
-              ("candle_history_finished", symbol, interval)
-            )
-          await asyncio.sleep(5)
-
-
+            interval = i.value
+            time = gen_timestring(interval, window_length)
+            if shutdown_flag:
+                return
+            elif i == Options.Interval.second_2:
+                continue
+            else:
+                await download_window(symbol, interval, time)
+                await queue.put(
+                  ("candle_history_finished", symbol, interval)
+                )
+            await asyncio.sleep(5)
