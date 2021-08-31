4d956318230b7bbeea2d5369f7956af1b8e81201
TvanMeer <tvanmeer123@gmail.com>
2021-08-31 03:39:08 +0200

    Put symbol in separate file

---
+++ src/models/symbol.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from pydantic.types import constr
+
+from .options import Options
+from .window import Window
+
+
+class Symbol(BaseModel):
+    """Holds all data related to a symbol, such as `BTCUSDT`."""
+
+    name:    constr(strip_whitespace=True, to_lower=True, min_length=3, max_length=6)
+    windows: dict[Options.Interval, Window] = dict()
