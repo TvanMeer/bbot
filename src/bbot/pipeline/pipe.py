0b1135ae09246a52ce1c4fb6fbd253589330c2c7
TvanMeer <tvanmeer123@gmail.com>
2021-09-03 03:10:16 +0200

    Rewrite pipeline

--- src/bbot/pipeline/pipe.py
+++ src/bbot/pipeline/pipe.py
@@ -1,50 +1,195 @@
+# pylint: disable=no-name-in-module
+
 from abc import ABC, abstractmethod
-from typing import Any
-from pydantic import BaseModel
+from collections import Counter
+from datetime import datetime
+from enum import Enum
+from typing import Any, List, Tuple
 
-from models.database import DataBase, Window
+from models.database import ContentType, DataBase, Window
+from models.options import Options
+from models.timeframe import TimeFrame
 
+
+class PayloadBelongsToTimeframe(str, Enum):
+        CURRENT:  "CURRENT"
+        PREVIOUS: "PREVIOUS"
+        NEXT:     "NEXT"
+        FIRST:    "FIRST"
 
 class Pipeline(ABC):
-    """Abstract baseclass for all pipes in pipeline module."""
+    """Handles insertion of content in the database."""
 
-    def process(self, raw: Any, db: DataBase):
-        """Interface for all pipelines."""
+    
+    n_items_processed = Counter(
+        {
+            ContentType.CANDLE_HISTORY: 0,
+            ContentType.CANDLE_STREAM:  0,
+        }
+    )
 
-        window  = self.get_window(raw, db)
-        parsed  = self.parse(raw)
-        window_updated = self.insert_in_window(parsed, window)
-        db_updated = self.insert_in_db(window_updated, db)
-        return db_updated
+
+    def process(
+        self, 
+        symbol:      str, 
+        interval:    Options.Interval, 
+        contenttype: ContentType, 
+        payload:     Any, 
+        db:          DataBase
+    ) -> DataBase:
+        """Calls process_window() for the windows that should be updated."""
+
+        self.n_items_processed.update(contenttype)
+
+        def one_window(self):
+            db.symbols[symbol].windows[interval] = self.process_window(
+                contenttype, payload, db.symbols[symbol].windows[interval]
+            )
+
+        def all_windows(self):
+            for iv, w in db.symbols[symbol].windows.items():
+                db.symbols[symbol].windows[iv] = self.process_window(contenttype, payload, w)
+
+        apply_on_n_windows = {
+            ContentType.CANDLE_HISTORY: one_window,
+            ContentType.CANDLE_STREAM:  all_windows,
+        }
+        apply_on_n_windows[contenttype].__call__(self)
+        return db
+
+
+
+    def process_window(
+        self, 
+        contenttype: ContentType, 
+        payload:     Any, 
+        window:      Window
+    ) -> Window:
+        """Interface for all pipelines. Payload is inserted in window and window is returned."""
+
+        if not window._history_downloaded and not contenttype == ContentType.CANDLE_HISTORY:
+            pass
+
+        o, c = self.get_open_close_time(payload)
+        p    = self.in_timeframe(c, window)
+
+        if   p == PayloadBelongsToTimeframe.CURRENT:
+            window = self.update_timeframe(payload, window)
+
+        elif p == PayloadBelongsToTimeframe.PREVIOUS:
+            window = self.update_timeframe(payload, window, prev_timeframe=True)
+
+        elif p == PayloadBelongsToTimeframe.NEXT:
+            window = self.create_next_timeframe(window)
+            window = self.insert_first_time(payload, window)
+
+        elif p == PayloadBelongsToTimeframe.FIRST:
+            if o:
+                window = self.create_first_timeframe(o, c, window)
+                window = self.insert_first_time(payload, window)
+            else:
+                # No open time given, drop payload and return unmodified window
+                return window
+        else:
+            raise Exception("Could not determine in which timeframe payload belongs.")
+
+        return window
+
+    
+
+    def in_timeframe(self, close_time, window) -> str:
+        """Returns what timeframe the item belongs to."""
+
+        if not window.timeframes:
+            return PayloadBelongsToTimeframe.FIRST
+
+        tf = window.timeframes[-1]
+        delta = tf.close_time - tf.open_time
+
+        if close_time > tf.close_time + delta:
+            raise Exception("Error in timeframe creation.")
+
+        elif close_time > tf.close_time:
+            return PayloadBelongsToTimeframe.NEXT
+
+        elif close_time > tf.open_time:
+            return PayloadBelongsToTimeframe.CURRENT
+
+        elif close_time > tf.open_time - delta:
+            return PayloadBelongsToTimeframe.PREVIOUS
+        else:
+            raise Exception("Data leakage: data in pipeline lags behind.")
+  
+
+
+    def create_first_timeframe(self, open_time, close_time, window) -> Window:
+        new_timeframe = TimeFrame(
+            open_time=open_time, close_time=close_time
+        )
+        return window.timeframes.append(new_timeframe)
+
+
+    def create_next_timeframe(self, window) -> Window:
+        last = window.timeframes[-1]
+        new_timeframe = TimeFrame.create_next_timeframe(
+                last.open_time, last.close_time
+        )
+        return window.timeframes.append(new_timeframe)
+
 
 
     @abstractmethod
-    def get_window(self, raw: Any, db: DataBase) -> Window:
-        """Gets the window from the database, where `raw` should be inserted.
-        The window choice is based on the symbol field and time data fields in `raw`.
+    def get_open_close_time(self, payload: Any) -> Tuple[datetime, datetime]:
+        """Parse and return both open en close time in payload.
+        Returns two datetime objects, or (None, <datatime>) when no open-time
+        is available, like in realtime trade updates.
         """
 
-        raise NotImplementedError
+        pass
 
 
     @abstractmethod
-    def parse(self, raw: Any) -> BaseModel:
-        """Turns raw data into a Pydantic Basemodel."""
-
-        raise NotImplementedError
-
-
-    @abstractmethod
-    def insert_in_window(self, item: BaseModel, window: Window) -> Window:
-        """Inserts this model in the corresponding window.
-        Then inserts this window in the database and returns the updated database.
+    def insert_first_time(self, payload: Any, window: Window) -> Window:
+        """Insert payload in timeframe when timeframe is empty. E.g. a new
+        candle in a timeframe where timeframe.candle == None.
+        This function requires a window instead of a timeframe, to access
+        metadata in its corresponding window.
         """
 
-        raise NotImplementedError
+        pass
 
 
     @abstractmethod
-    def insert_in_db(self, window: Window, db: DataBase) -> DataBase:
-        """Inserts updated window in database."""
+    def update_timeframe(self, payload: Any, window: Window, prev_timeframe = False) -> Window:
+        """Update the current, or previous timeframe."""
 
-        raise NotImplementedError
+        pass
+
+
+
+class HistoricalCandlePipe(Pipeline):
+    """ """
+
+    def get_open_close_time(self, payload: List) -> Tuple[datetime, datetime]:
+        pass
+
+    def insert_first_time(self, payload: List, window: Window) -> Window:
+        pass
+
+    def update_timeframe(self, payload: Any, window: Window, prev_timeframe) -> Window:
+        raise Exception("Trying to update timeframe in historical data.")
+
+
+
+class StreamCandlePipe(Pipeline):
+    """ """
+
+    def get_open_close_time(self, payload: dict) -> Tuple[datetime, datetime]:
+        pass
+
+    def insert_first_time(self, payload: dict, window: Window) -> Window:
+        pass
+
+    def update_timeframe(self, payload: dict, window: Window, prev_timeframe) -> Window:
+        pass
+        
