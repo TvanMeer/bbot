1561321cb6529b3491cf8dd1f34a95e520004738
TvanMeer <tvanmeer123@gmail.com>
2021-09-11 04:28:36 +0200

    Redesign pipeline and reimpement candle related pipes

--- src/bbot/pipeline/pipe.py
+++ src/bbot/pipeline/pipe.py
@@ -2,21 +2,14 @@
 
 from abc import ABC, abstractmethod
 from collections import Counter
-from datetime import datetime
-from enum import Enum
+from datetime import datetime, timedelta
 from typing import Any, List, Tuple
 
+from ..constants import *
 from models.candle import Candle
 from models.database import ContentType, DataBase, Window
 from models.options import Options
 from models.timeframe import TimeFrame
-
-
-class PayloadBelongsToTimeframe(str, Enum):
-        CURRENT:  "CURRENT"
-        PREVIOUS: "PREVIOUS"
-        NEXT:     "NEXT"
-        FIRST:    "FIRST"
 
 class Pipeline(ABC):
     """Handles insertion of content in the database."""
@@ -33,7 +26,7 @@ class Pipeline(ABC):
     def process(
         self, 
         symbol:      str, 
-        interval:    Options.Interval, 
+        interval:    Interval, 
         contenttype: ContentType, 
         payload:     Any, 
         db:          DataBase
@@ -51,13 +44,12 @@ class Pipeline(ABC):
             for iv, w in db.symbols[symbol].windows.items():
                 db.symbols[symbol].windows[iv] = self.process_window(contenttype, payload, w)
 
-        apply_on_n_windows = {
+        apply_on_windows = {
             ContentType.CANDLE_HISTORY: one_window,
             ContentType.CANDLE_STREAM:  all_windows,
         }
-        apply_on_n_windows[contenttype].__call__(self)
+        apply_on_windows[contenttype]()
         return db
-
 
 
     def process_window(
@@ -71,38 +63,26 @@ class Pipeline(ABC):
         if not window._history_downloaded and not contenttype == ContentType.CANDLE_HISTORY:
             pass
 
-        o, c = self.get_open_close_time(payload)
-        p    = self.in_timeframe(c, window)
+        tf = {
+            InTimeFrame.FIRST: self.first,
+            InTimeFrame.PREVIOUS: self.previous,
+            InTimeFrame.CURRENT: self.current,
+            InTimeFrame.NEXT: self.nexxt,
+            InTimeFrame.OTHER: self.data_leakage_error,
+        }
 
-        if   p == PayloadBelongsToTimeframe.CURRENT:
-            window = self.update_timeframe(payload, window)
+        close_time = self.get_close_time(payload)
+        in_tf = self.which_timeframe(close_time, window)
 
-        elif p == PayloadBelongsToTimeframe.PREVIOUS:
-            window = self.update_timeframe(payload, window, prev_timeframe=True)
+        return tf[in_tf](payload, window)
 
-        elif p == PayloadBelongsToTimeframe.NEXT:
-            window = self.create_next_timeframe(window)
-            window = self.insert_in_next_timeframe(payload, window)
 
-        elif p == PayloadBelongsToTimeframe.FIRST:
-            if o:
-                window = self.create_first_timeframe(o, c, window)
-                window = self.insert_first_time(payload, window)
-            else:
-                # No open time given, drop payload and return unmodified window
-                return window
-        else:
-            raise Exception("Could not determine in which timeframe payload belongs.")
 
-        return window
-
-    
-
-    def in_timeframe(self, close_time, window) -> str:
-        """Returns what timeframe the item belongs to."""
+    def which_timeframe(self, close_time, window) -> str:
+        """Returns which timeframe the item belongs to."""
 
         if not window.timeframes:
-            return PayloadBelongsToTimeframe.FIRST
+            return InTimeFrame.FIRST
 
         tf = window.timeframes[-1]
         delta = tf.close_time - tf.open_time
@@ -110,48 +90,40 @@ class Pipeline(ABC):
         if close_time > tf.close_time + delta:
             raise Exception("Error in timeframe creation.")
 
-        elif close_time > tf.close_time:
-            return PayloadBelongsToTimeframe.NEXT
+        if close_time > tf.close_time:
+            return InTimeFrame.NEXT
 
-        elif close_time > tf.open_time:
-            return PayloadBelongsToTimeframe.CURRENT
+        if close_time > tf.open_time:
+            return InTimeFrame.CURRENT
 
-        elif close_time > tf.open_time - delta:
-            return PayloadBelongsToTimeframe.PREVIOUS
-        else:
-            raise Exception("Data leakage: data in pipeline lags behind.")
+        if close_time > tf.open_time - delta:
+            return InTimeFrame.PREVIOUS
+
+        return InTimeFrame.OTHER
   
 
 
-    def create_first_timeframe(self, open_time, close_time, window) -> Window:
-        new_timeframe = TimeFrame(
-            open_time=open_time, close_time=close_time
-        )
-        return window.timeframes.append(new_timeframe)
+    def data_leakage_error(self):
+        e = """Data leakage: bbot cannot process the data fast enough. 
+        Reduce the number of data sources or try to increase the performance
+        of your feature calculation functions.
+        """
 
-
-    def create_next_timeframe(self, window) -> Window:
-        last = window.timeframes[-1]
-        new_timeframe = TimeFrame.create_next_timeframe(
-                last.open_time, last.close_time
-        )
-        return window.timeframes.append(new_timeframe)
-
+        raise Exception(e)
 
 
     @abstractmethod
-    def get_open_close_time(self, payload: Any) -> Tuple[datetime, datetime]:
-        """Parse and return both open en close time in payload.
-        Returns two datetime objects, or (None, <datatime>) when no open-time
-        is available, like in realtime trade updates.
+    def get_close_time(self, payload: Any) -> datetime:
+        """Parse and return close time in payload.
+        Returns datetime object.
         """
 
         pass
 
 
     @abstractmethod
-    def insert_first_time(self, payload: Any, window: Window) -> Window:
-        """Insert payload in timeframe when timeframe is empty. E.g. a new
+    def first(self, payload: Any, window: Window) -> Window:
+        """Inserts payload in timeframe when timeframe is empty. E.g. a new
         candle in a timeframe where timeframe.candle == None.
         This function requires a window instead of a timeframe, to access
         metadata in its corresponding window.
@@ -159,62 +131,163 @@ class Pipeline(ABC):
 
         pass
 
+
     @abstractmethod
-    def insert_in_next_timeframe(self, payload: Any, window: Window) -> Window:
-        """Insert payload in next timeframe in the sequence of timeframes."""
+    def previous(self, payload: Any, window: Window) -> Window:
+        """Inserts payload in previous timeframe. 
+        (if the close time in payload < open time of current timeframe)
+        """
+
+        pass
+
+    @abstractmethod
+    def current(self, payload: Any, window: Window) -> Window:
+        """Updates the current timeframe. (where current is window.timeframes[-1])
+        """
 
         pass
 
 
     @abstractmethod
-    def update_timeframe(self, payload: Any, window: Window, prev_timeframe = False) -> Window:
-        """Update the current, or previous timeframe."""
+    def nexxt(self, payload: Any, window: Window) -> Window:
+        """Creates next timeframe in window.timeframes and
+        inserts payload in this new timeframe.
+        """
 
         pass
 
+
+    # Helperfuncs
+
+    def round_time(self, close_time: datetime):
+        """Rounds time to closest possible 2-second close time -1.
+        Used to get 2 second candle close time based on event time.
+        """
+      
+        return close_time - timedelta(microseconds=close_time.microsecond) - timedelta(milliseconds=1)
 
 
 class HistoricalCandlePipe(Pipeline):
 
-    def get_open_close_time(self, payload: List) -> Tuple[datetime, datetime]:
+    def get_close_time(self, payload: List, window: Window) -> datetime:
+        return datetime(payload[6])
+
+
+    def first(self, payload: List, window: Window) -> Window:
         o = datetime(payload[0])
         c = datetime(payload[6])
-        return (o, c)
-
-
-    def insert_first_time(self, payload: List, window: Window) -> Window:
+        timeframe = TimeFrame(open_time=o, close_time=c)
+        window.timeframes[0] = timeframe
         candle = Candle.parse_historical_candle(payload)
-        window.timeframes[-1].candle = candle
+        window.timeframes[0].candle = candle
         return window
 
 
-    def insert_in_next_timeframe(self, payload: List, window: Window) -> Window:
-        generated_tf = window.timeframes[-1]
-        o, c = self.get_open_close_time(payload)
-        if generated_tf.open_time != o or generated_tf.close_time != c:
-            raise Exception("Inconsistent open and close times in historical candle data.")
+    def previous(self, payload: List, window: Window) -> Window:
+        raise Exception("Tried to insert historical candle in previous timeframe.")
 
+
+    def current(self, payload: List, window: Window) -> Window:
+        raise Exception("Tried to insert historical candle in current timeframe.")
+
+
+    def nexxt(self, payload: List, window: Window) -> Window:
+        o = datetime(payload[0])
+        c = datetime(payload[6])
+        timeframe = TimeFrame(open_time=o, close_time=c)
+        window.timeframes.append(timeframe)
         candle = Candle.parse_historical_candle(payload)
         window.timeframes[-1].candle = candle
         return window
-
-
-    def update_timeframe(self, payload: Any, window: Window, prev_timeframe: bool) -> Window:
-        raise Exception("Trying to update timeframe in historical data.")
-
 
 
 class StreamCandlePipe(Pipeline):
 
-    def get_open_close_time(self, payload: dict) -> Tuple[datetime, datetime]:
-        pass
+    # required by super
+    def get_close_time(self, payload: dict, window: Window) -> datetime:
+        return super().round_time(datetime(payload["E"]))
 
-    def insert_first_time(self, payload: dict, window: Window) -> Window:
-        pass
 
-    def insert_in_next_timeframe(self, payload: dict, window: Window) -> Window:
-        pass
+    def set_last_update(self, payload: dict, window: Window) -> Window:
+        candle_1m = Candle.parse_candle(payload)
+        closed = bool(payload["x"])
+        window._last_candle_update = candle_1m
+        window._last_candle_update_closed = closed
+        return window
 
-    def update_timeframe(self, payload: dict, window: Window, prev_timeframe: bool) -> Window:
-        pass
+
+    # required by super
+    def first(self, payload: dict, window: Window) -> Window:
+
+        if not window._last_candle_update:
+            window = self.set_last_update(payload, window)
+            return window
+
+        if window.interval == Interval.SECOND_2:
+            c = super().round_time(datetime(payload["E"]))
+            o = c - timedelta(milliseconds=1999)
+            timeframe = TimeFrame(open_time=o, close_time=c)
+            window.timeframes[0] = timeframe
+            candle_1m = Candle.parse_candle(payload)
+            candle_2s = Candle.create_2s_candle(
+                candle_1m, 
+                window._last_candle_update, 
+                window._last_candle_update_closed
+            )
+            window.timeframes[0].candle = candle_2s
+            window = self.set_last_update(payload, window)
+            return window
+
+        else:
+            raise Exception("Tried to insert candle from stream before history downloaded.")
         
+
+    def update(self, payload, window, idx) -> Window:
+        window.timeframes[idx].candle = Candle.update(
+            candle=window.timeframes[idx].candle, 
+            update=Candle.parse_candle(payload), 
+            previous_update=window._last_candle_update,
+            previous_update_closed=window._last_candle_update_closed
+        )
+        window = self.set_last_update(payload, window)
+        return window
+
+
+    # required by super
+    def previous(self, payload: dict, window: Window) -> Window:
+        if window.interval == Interval.SECOND_2:
+            raise Exception("Tried to update previous 2 second timeframe.")
+        window = self.update(payload, window, -2)
+        return window
+
+
+    # required by super
+    def current(self, payload: dict, window: Window) -> Window:
+        if window.interval == Interval.SECOND_2:
+            raise Exception("Tried to update 2 second timeframe.")
+        window = self.update(payload, window, -1)
+        return window
+
+
+    def add_new_timeframe(self, window) -> Window:
+        timeframe = TimeFrame.create_next_timeframe(
+            window.timeframes[-1].open_time, 
+            window.timeframes[-1].close_time
+        )
+        window.timeframes.append(timeframe)
+        return window
+
+
+    # required by super
+    def nexxt(self, payload: dict, window: Window) -> Window:
+        candle = Candle.parse_candle(payload)
+        if window.interval == Interval.SECOND_2:
+            candle = Candle.create_2s_candle(
+                candle, 
+                window._last_candle_update, 
+                window._last_candle_update_closed
+            )
+        window = self.add_new_timeframe(window)
+        window.timeframes[-1].candle = candle
+        window = self.set_last_update(payload, window)
+        return window
